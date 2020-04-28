ba3869ca5b366649bc799712d2518550223c3a7c
wxnn <wxn233@outlook.com>
2020-04-28 16:22:15 +0800

    add multiprocess content

--- test.py
+++ test.py
@@ -1,27 +1,44 @@
-import threading
+from multiprocessing import Process, Semaphore, Lock, Queue
 import time
 
-
-def target(second):
-    # 线程的名字就是通过threading.current_thread().name获得，主线程的名字为MainThreat，子线程的名字为Thread-num
-    print(f'threat {threading.current_thread().name} is running')
-    print(f'threat {threading.current_thread().name} sleep {second}s')
-    time.sleep(second)
-    print(f'threat {threading.current_thread().name} is ended')
+buffer = Queue(10)    # 定义一个共享队列
+empty = Semaphore(2)  # 定义信号量，代表缓冲区空余数
+full = Semaphore(0)   # 定义信号量，代表缓冲区占用数
+lock = Lock()
 
 
-print(f'Threading {threading.current_thread().name} is running')
-for i in [1, 5]:
-    # 使用threading.Tread()类新建了两个线程，target参数就是刚才定义的target函数，args通过列表的形式传递
-    thread = threading.Thread(target=target, args=[i])
-    # 新建线程声明之后，使用start()方法即可开始线程的运行
-    thread.start()
-print(f'Threading {threading.current_thread().name} is ended')
+class Consumer(Process):
+    def run(self):
+        global buffer, empty, full, lock
+        while True:
+            full.acquire()  # 使用acquire 占用一个缓冲区位置，缓冲区空余数减一
+            lock.acquire()  # 加锁
+            buffer.get()   # 对缓冲区进行操作
+            print('Consumer pop an element')
+            time.sleep(1)
+            lock.release()  # 释放锁
+            empty.release()
 
-threads = []
-for i in [1, 5]:
-    thread = threading.Thread(target=target, args=[i])
-    threads.append(thread)
-    thread.start()
-for thread in threads:
-    thread.join()
+
+class Producer(Process):
+    def run(self):
+        global buffer, empty, full, lock
+        while True:
+            empty.acquire()
+            lock.acquire()
+            buffer.put(1)
+            print('Producer append an element')
+            time.sleep(1)
+            lock.release()
+            full.release()
+
+
+if __name__ == '__main__':
+    p = Producer()
+    c = Consumer()
+    p.daemon = c.daemon = True
+    p.start()
+    c.start()
+    p.join()
+    c.join()
+    print('Main Process Ended')
